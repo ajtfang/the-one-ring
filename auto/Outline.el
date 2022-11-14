3a4961f1bee698163b85acfa241118872cd4aaee
shofly12 <107259837+ajtfang@users.noreply.github.com>
2022-11-14 10:31:10 -0500

    Setting up git
    Added outline.tex which contains some random thoughts, plus a the spectral theory article from Sam

---
+++ auto/Outline.el
@@ -0,0 +1,20 @@
+(TeX-add-style-hook
+ "Outline"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("biblatex" "sortcites" "backend=biber" "date=year" "style=numeric-comp" "doi=false" "isbn=false" "url=false" "eprint=true")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "Ring"
+    "biblatex"))
+ :latex)
+
---
+++ auto/Ring.el
@@ -0,0 +1,82 @@
+(TeX-add-style-hook
+ "Ring"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("xcolor" "dvipsnames") ("hyperref" "bookmarksopen=true" "colorlinks" "linkcolor=blue" "citecolor=blue")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "geometry"
+    "array"
+    "paralist"
+    "verbatim"
+    "amsfonts"
+    "graphicx"
+    "amssymb"
+    "mathtools"
+    "mathabx"
+    "mathrsfs"
+    "amsthm"
+    "xr-hyper"
+    "xcolor"
+    "hyperref"
+    "cleveref"
+    "tensor"
+    "cancel"
+    "pdflscape"
+    "appendix"
+    "enumitem")
+   (TeX-add-symbols
+    "LHS"
+    "RHS"
+    "Real"
+    "Complex"
+    "Natural"
+    "p"
+    "Ker"
+    "coKer"
+    "Range"
+    "UnitSphere"
+    "Laplace"
+    "ImagUnit"
+    "bOne"
+    "Identity"
+    "BumpFunction"
+    "supp"
+    "Span"
+    "residue"
+    "local"
+    "Distribution"
+    "Boundary"
+    "rank"
+    "Divergence"
+    "Curl"
+    "IdentityMatrix"
+    "SolutionOp"
+    "InfGen"
+    "InteriorProd"
+    "Interval"
+    "Hom"
+    "myDate")
+   (LaTeX-add-mathtools-DeclarePairedDelimiters
+    '("bangle" "")
+    '("abs" "")
+    '("norm" "")
+    '("squareBrace" "")
+    '("curlyBrace" "")
+    '("PoissonB" "")
+    '("evalAt" ""))
+   (LaTeX-add-amsthm-newtheorems
+    "theorem"
+    "prop"
+    "lemma"
+    "remark"
+    "definition"
+    "corollary"))
+ :latex)
+
